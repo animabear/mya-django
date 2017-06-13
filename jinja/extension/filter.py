adb64130f0f026082170fa747dcb19e0f875f922
xiongjunjie.frontend <xiongjunjie.frontend@bytedance.com>
2017-06-13 18:27:14 +0800

    替换filter json处理函数为ujson

--- jinja/extension/filter.py
+++ jinja/extension/filter.py
@@ -1,6 +1,6 @@
 # coding=utf-8
-
+import ujson
 from jinja2.utils import htmlsafe_json_dumps
 
 def jsonify(value):
-    return htmlsafe_json_dumps(value)
+    return htmlsafe_json_dumps(value, ujson.dumps)
